452e4a90699e5e2b7a8b5405ab566384cb3871f1
filip-daca <filip.hubert.daca@gmail.com>
2020-11-22 16:47:45 +0100

    Refactor and add randomness

---
+++ src/main/groovy/reaction/JohnDeereReaction.groovy
@@ -0,0 +1,35 @@
+package reaction
+
+import discord4j.common.util.Snowflake
+import discord4j.core.object.entity.Message
+import discord4j.core.object.reaction.ReactionEmoji
+import reactor.core.publisher.Flux
+
+class JohnDeereReaction {
+
+    static johnDeereEmoji = ReactionEmoji.custom(Snowflake.of(640977678643494914),
+            "john_deere", false)
+    static phrases = [
+            'john deere',
+            'johndeere'
+    ]
+    static responses = [
+            'Tomorrow morning, we\'re mowing the lawn!',
+            'Green machine goes "brrrrrrrrr"!'
+    ]
+
+    static consume(Flux messages) {
+        def johnDeereMessages = messages
+                .filter(message -> phrases.contains(message.getContent().toLoweCase()))
+
+        johnDeereMessages
+                .flatMap(message -> message.addReaction(johnDeereEmoji))
+                .subscribe()
+
+        johnDeereMessages
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage(responses.shuffled().first()))
+                .subscribe()
+    }
+
+}
--- src/main/groovy/xyz/komeo/BoomerBot.groovy
+++ src/main/groovy/xyz/komeo/BoomerBot.groovy
@@ -4,9 +4,9 @@ import discord4j.common.util.Snowflake
 import discord4j.core.DiscordClientBuilder
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
-import discord4j.core.object.entity.GuildEmoji
 import discord4j.core.object.entity.Message
-import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.object.reaction.ReactionEmoji
+import reaction.JohnDeereReaction;
 
 class BoomerBot {
 
@@ -27,18 +27,18 @@ class BoomerBot {
                 .flatMap(channel -> channel.createMessage("Howdy partner!"))
                 .subscribe()
 
-        client.getEventDispatcher().on(MessageCreateEvent)
+        def validMessages = client.getEventDispatcher().on(MessageCreateEvent)
                 .map(MessageCreateEvent::getMessage)
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message -> message.getContent().toLowerCase().contains("boomer"))
                 .filter(message -> !message.getContent().startsWith("!"))
+
+        validMessages
+                .filter(message -> message.getContent().toLowerCase().contains("boomer"))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage("Are you winning son?"))
                 .subscribe()
 
-        client.getEventDispatcher().on(MessageCreateEvent)
-                .map(MessageCreateEvent::getMessage)
-                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+        validMessages
                 .filter(message ->
                         message.getContent().toLowerCase().contains("boomster") ||
                                 message.getContent().toLowerCase().contains("boomer") ||
@@ -47,34 +47,11 @@ class BoomerBot {
                                 message.getContent().toLowerCase().contains("good vibe") ||
                                 message.getContent().toLowerCase().contains("nice")
                 )
-                .filter(message -> !message.getContent().startsWith("!"))
                 .flatMap(message -> message.addReaction(ReactionEmoji.custom(Snowflake.of(640976634589085726),
                         "boomster_white", false)))
                 .subscribe()
 
-        client.getEventDispatcher().on(MessageCreateEvent)
-                .map(MessageCreateEvent::getMessage)
-                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message ->
-                        message.getContent().toLowerCase().contains("john deere") ||
-                                message.getContent().toLowerCase().contains("johndeere")
-                )
-                .filter(message -> !message.getContent().startsWith("!"))
-                .flatMap(message -> message.addReaction(ReactionEmoji.custom(Snowflake.of(640977678643494914),
-                        "john_deere", false)))
-                .subscribe()
-
-        client.getEventDispatcher().on(MessageCreateEvent)
-                .map(MessageCreateEvent::getMessage)
-                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message ->
-                        message.getContent().toLowerCase().contains("john deere") ||
-                                message.getContent().toLowerCase().contains("johndeere")
-                )
-                .filter(message -> !message.getContent().startsWith("!"))
-                .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Tomorrow morning, we're mowing the lawn!"))
-                .subscribe()
+        JohnDeereReaction.consume(validMessages)
 
         client.onDisconnect().block()
     }
