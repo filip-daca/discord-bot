60a0f0a500515e194a7bb0d088c06e85cbe19ebd
filip-daca <filip.hubert.daca@gmail.com>
2020-11-22 15:05:49 +0100

    Boomer won't look inside commands

--- src/main/groovy/xyz/komeo/BoomerBot.groovy
+++ src/main/groovy/xyz/komeo/BoomerBot.groovy
@@ -11,26 +11,27 @@ class BoomerBot {
         def client = DiscordClientBuilder.create(System.getenv('ACCESS_TOKEN')).build().login().block();
 
         client.getEventDispatcher().on(ReadyEvent)
-            .subscribe(event -> {
-                def self = event.getSelf();
-                println "Logged in as ${self.getUsername()} ${self.getDiscriminator()}"
-            });
+                .subscribe(event -> {
+                    def self = event.getSelf();
+                    println "Logged in as ${self.getUsername()} ${self.getDiscriminator()}"
+                });
 
         client.getEventDispatcher().on(MessageCreateEvent)
-            .map(MessageCreateEvent::getMessage)
-            .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-            .filter(message -> message.getContent().equalsIgnoreCase("!boomer"))
-            .flatMap(Message::getChannel)
-            .flatMap(channel -> channel.createMessage("Howdy partner!"))
-            .subscribe();
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!boomer"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Howdy partner!"))
+                .subscribe();
 
         client.getEventDispatcher().on(MessageCreateEvent)
-            .map(MessageCreateEvent::getMessage)
-            .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-            .filter(message -> message.getContent().contains("boomer"))
-            .flatMap(Message::getChannel)
-            .flatMap(channel -> channel.createMessage("Are you winning son?"))
-            .subscribe();
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().contains("boomer"))
+                .filter(message -> !message.getContent().startsWith("!"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Are you winning son?"))
+                .subscribe();
 
         client.onDisconnect().block();
     }
