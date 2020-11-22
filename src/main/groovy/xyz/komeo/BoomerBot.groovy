a1864ba6ce64dc35e93ac2ebdbb8718c82d78501
filip-daca <filip.hubert.daca@gmail.com>
2020-11-22 15:54:32 +0100

    Try reactions

--- src/main/groovy/xyz/komeo/BoomerBot.groovy
+++ src/main/groovy/xyz/komeo/BoomerBot.groovy
@@ -1,9 +1,12 @@
 package xyz.komeo
 
+import discord4j.common.util.Snowflake
 import discord4j.core.DiscordClientBuilder
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
-import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.GuildEmoji
+import discord4j.core.object.entity.Message
+import discord4j.core.object.reaction.ReactionEmoji;
 
 class BoomerBot {
 
@@ -31,6 +34,22 @@ class BoomerBot {
                 .filter(message -> !message.getContent().startsWith("!"))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage("Are you winning son?"))
+                .subscribe()
+
+        client.getEventDispatcher().on(MessageCreateEvent)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message ->
+                        message.getContent().toLowerCase().contains("boomster") ||
+                                message.getContent().toLowerCase().contains("boomer") ||
+                                message.getContent().toLowerCase().contains("monster") ||
+                                message.getContent().toLowerCase().contains("good time") ||
+                                message.getContent().toLowerCase().contains("good vibe") ||
+                                message.getContent().toLowerCase().contains("nice")
+                )
+                .filter(message -> !message.getContent().startsWith("!"))
+                .flatMap(message -> message.addReaction(ReactionEmoji.custom(Snowflake.of(640976634589085726),
+                        "boomster_white", false)))
                 .subscribe()
 
         client.onDisconnect().block()
