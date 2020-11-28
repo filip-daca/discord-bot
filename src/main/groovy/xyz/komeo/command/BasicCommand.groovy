157668967d1f82149b8c562df1e8461e1a952450
filip-daca <filip.hubert.daca@gmail.com>
2020-11-28 12:21:08 +0100

    Basic command to speak with boomer

--- src/main/groovy/xyz/komeo/command/BasicCommand.groovy
+++ src/main/groovy/xyz/komeo/command/BasicCommand.groovy
@@ -3,11 +3,18 @@ package xyz.komeo.command
 import discord4j.core.object.entity.Message
 import reactor.core.publisher.Flux
 import xyz.komeo.flow.ConsumesFlux
+import xyz.komeo.flow.Lex
+import xyz.komeo.flow.Lexer
 
 class BasicCommand implements ConsumesFlux {
 
+    static lexes = [
+            new Lex(["hell|hi|howd"])
+    ]
+
     def consume(Flux messages) {
         messages
+                .filter(message -> Lexer.messageMatches(message.getContent(), lexes))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage("Howdy partner!"))
                 .subscribe()
